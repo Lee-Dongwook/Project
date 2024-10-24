81a37e76b919afaf73cc9ccfa441d1406c403ccb
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-24 20:12:51 +0900

    hotfix : bcrypt -> bcryptjs로 변환

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.html$/,
+      use: 'ignore-loader',
+    });
+    config.externals = [...config.externals, 'bcrypt'];
+    return config;
+  },
+};
 
 export default nextConfig;
